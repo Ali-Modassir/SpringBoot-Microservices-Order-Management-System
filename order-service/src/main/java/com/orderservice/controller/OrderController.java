e5aeff224ca87d93bcc6eb6e51a02b71d1735bbb
Modassir <Modassir@rs-MacBook-Pro.local>
2023-01-31 19:51:12 +0530

    added circuit breaker

--- order-service/src/main/java/com/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/orderservice/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.orderservice.controller;
 import com.orderservice.dto.OrderRequest;
 import com.orderservice.dto.OrderResponse;
 import com.orderservice.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -19,8 +20,13 @@ public class OrderController {
 
    @PostMapping("/create")
    @ResponseStatus(HttpStatus.CREATED)
+   @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
    public String placeOrder(@RequestBody OrderRequest orderRequest) {
        return orderService.placeOrder(orderRequest) ;
+   }
+
+   public String fallbackMethod(OrderRequest orderRequest,RuntimeException runtimeException){
+        return "Oops!!, something went wrong" ;
    }
 
 //   @GetMapping("/all")
